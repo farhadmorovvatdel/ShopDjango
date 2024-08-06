f9e4345447195bcc367b0fd838cbfd2a633a1818
farhad morovvatdel <f.morovvatdel@gmail.com>
2024-08-06 23:46:26 +0330

    add modeladmin

--- Category/admin.py
+++ Category/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Category
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','parent')
+    list_display = ('name',)
     list_filter = ('name',)
 
 
