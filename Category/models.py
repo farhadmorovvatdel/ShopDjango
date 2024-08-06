c79066b16b78473595ff2f69e1248ab99923f003
farhad morovvatdel <f.morovvatdel@gmail.com>
2024-08-06 23:47:16 +0330

    edit models

--- Category/models.py
+++ Category/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100,verbose_name="نام")
-    parent=models.ForeignKey('Category', on_delete=models.CASCADE,
-        related_name='subcategories', blank=True, null=True,verbose_name="زیر مجموعه")
+    # parent=models.ForeignKey('Category', on_delete=models.CASCADE,
+    #     related_name='subcategories', blank=True, null=True,verbose_name="زیر مجموعه")
     def __str__(self):
         return self.name
     class Meta:
